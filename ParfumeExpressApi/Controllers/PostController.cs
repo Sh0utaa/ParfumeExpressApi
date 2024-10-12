dab5935e99399d48ffcb8d3c42ba61fd0ec9c0b2
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-10-12 18:26:22 +0400

    Making Post Controller, need to add DTOs

---
+++ ParfumeExpressApi/Controllers/PostController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParfumeExpressApi.Interfaces;
+
+namespace ParfumeExpressApi.Controllers
+{
+    [Route("api/post")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            if (!ModelState.IsValid) { 
+                return BadRequest(ModelState);
+            }
+
+            var Posts = await _postRepository.GetAllAsync();
+
+            return Ok(Posts);
+        }
+
+    }
+}
