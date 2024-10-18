6df6b127c0a930d04cdc9ff5646b26d21ba1703f
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-10-18 20:14:23 +0400

    Mostly finished the backend time to start working on the frontend

---
+++ ParfumeExpressMvc/wwwroot/js/registrationAuthentication.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('#registerForm').on('submit', function (e) {
+        e.preventDefault();
+        const registerData = {
+            email: $('input[name="email"]').val(),
+            password: $('input[name="password"]').val(),
+        };
+        $.ajax({
+            url: 'https://localhost:7046/register',
+            type: 'POST',
+            contentType: 'application/json',
+
+            data: JSON.stringify(registerData),
+            success: function (response) {
+                // Handle success (e.g., store token, redirect)
+                console.log(response);
+                alert('Registration successful');
+            },
+            error: function () {
+                alert('Invalid Registration');
+                console.log(response);
+            }
+        });
+    });
+});
